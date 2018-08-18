55cfaec02d2e8b2ace901f2dad54482c4a8751cd
n-inja <mccreepertakumi@gmail.com>
2018-08-19 01:34:16 +0900

    server

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"./utils"
 	"fmt"
 	"os"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main () {
@@ -11,10 +13,60 @@ func main () {
 	if os.Getenv("DATABASE_ADDRESS") != "" {
 		databaseAddress = os.Getenv("DATABASE_ADDRESS")
 	}
-	err := db.Open(os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), "tcp("+databaseAddress+")", os.Getenv("DATABASE_NAME"))
+	err := utils.Open(os.Getenv("DATABASE_USERNAME"), os.Getenv("DATABASE_PASSWORD"), "tcp("+databaseAddress+")", os.Getenv("DATABASE_NAME"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer db.Close()
+	router := gin.Default()
+	router.POST("/go-login/api/v1/login", login)
+	router.POST("/go-login/api/v1/logout", logout)
+	router.POST("/go-login/api/v1/users", register)
+	router.PUT("/go-login/api/v1/users", change)
+	router.DELETE("/go-login/api/v1/users", ban)
+	router.StaticFS("/login", http.Dir("static"))
+	router.Run(":8080")
+}
+
+type LoginPost struct {
+	ID string `form:"id" binding:"required"`
+	Password string `form:"password" binding:"required"`
+}
+
+func login (c *gin.Context) {
+	var loginPost LoginPost
+	err := c.BindJSON(&loginPost)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "bad request",
+		})
+		return
+	}
+	session, err := utils.StartSession(loginPost.ID, loginPost.Password)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"message": "incorrect id or password",
+		})
+		return
+	} else {
+		c.SetCookie("session", session, 1, "/", os.Getenv("DOMAIN"), true, true)
+		c.JSON(http.StatusOK, gin.H{})
+		return
+	}
+}
+
+func logout (c *gin.Context) {
+
+}
+
+func register (c *gin.Context) {
+
+}
+
+func ban (c *gin.Context) {
+
+}
+
+func change (c *gin.Context) {
+
 }
--- utils/db.go
+++ utils/db.go
@@ -1,10 +1,13 @@
-package db
+package utils
 
 import (
 	"golang.org/x/crypto/bcrypt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/xid"
 	"database/sql"
 	"os"
+	"errors"
+	"time"
 )
 
 type User struct {
@@ -35,6 +38,19 @@ func initDB () (error) {
 	}
 	if !rows.Next() {
 		_, err := db.Exec("create table users (id varchar(32) NOT NULL PRIMARY KEY, name varchar(32), hash varchar(256))")
+		if err != nil {
+			rows.Close()
+			return err
+		}
+	}
+	rows.Close()
+
+	rows, err = db.Query("show tables like 'sessions'")
+	if err != nil {
+		return err
+	}
+	if !rows.Next() {
+		_, err := db.Exec("create table sessions (id varchar(32) NOT NULL, session varchar(20) NOT NULL PRIMARY KEY, expiration_date timestamp NOT NULL, index(expiration_date))")
 		if err != nil {
 			rows.Close()
 			return err
@@ -84,30 +100,64 @@ func Comfirm (ID, password string) (bool) {
 	return ret == nil
 }
 
-func ChangePassword (ID, oldPass, newPass string) (bool) {
+func ChangePassword (ID, oldPass, newPass string) (error) {
 	if !Comfirm(ID, oldPass) {
-		return false
+		return errors.New("incorrect id or password")
 	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(newPass), bcrypt.DefaultCost)
 	if err != nil {
-		return false
+		return err
 	}
 	_, err = db.Exec("update users set hash = ? where id = ?", hash, ID)
-	return err == nil
+	return err
 }
 
-func ChangeName (ID, password, name string) (bool) {
+func ChangeName (ID, password, name string) (error) {
 	if !Comfirm(ID, password) {
-		return false
+		return errors.New("incorrect id or password")
 	}
 	_, err := db.Exec("update users set name = ? where id = ?", name, ID)
-	return err == nil
+	return err
 }
 
-func Delete (ID string) (bool) {
+func Delete (ID string) (error) {
 	if ID == "root" {
-		return false
+		return errors.New("permission denied")
 	}
 	_, err := db.Exec("delete from users where id = ?", ID)
-	return err == nil
+	return err
+}
+
+func StartSession (ID, password string) (string, error) {
+	if !Comfirm(ID, password) {
+		return "", errors.New("incorrect id or password")
+	}
+	expiration := time.Now().AddDate(0, 0, 30)
+	session := xid.New()
+	_, err := db.Exec("insert into sessions values(?, ?, ?)", ID, session.String(), expiration.Format("2006-01-02 15:04:05"))
+	if err != nil {
+		return "", err
+	}
+	return session.String(), nil
+}
+
+func CheckSession (session string) (string, error) {
+	now := time.Now()
+	rows, err := db.Query("select id from sessions where session = ? and expiration_time > ?", session, now.Format("2016-01-02 15:04:05"))
+	if err != nil {
+		return "", err
+	}
+	defer rows.Close()
+	if !rows.Next() {
+		return "", nil
+	}
+	var ID string
+	rows.Scan(&ID)
+	return ID, nil
+}
+
+func DiscardSession (session string) (error) {
+	now := time.Now()
+	_, err := db.Exec("delete from sesssions where session = ? and expiration_time > ?", session, now.Format("2016-01-02 15:04:05"))
+	return err
 }
