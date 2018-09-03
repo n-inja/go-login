5c127e42db458b1e863e74ba8b9478a69e10a261
n-inja <mccreepertakumi@gmail.com>
2018-09-03 21:46:11 +0900

    erase foreign key

--- utils/db.go
+++ utils/db.go
@@ -46,7 +46,7 @@ func initDB() error {
 		return err
 	}
 	if !rows.Next() {
-		_, err := db.Exec("create table users (id varchar(32) NOT NULL PRIMARY KEY, name varchar(32) NOT NULL, hash varchar(256) NOT NULL, auth varchar(20) NOT NULL) engine=innodb")
+		_, err := db.Exec("create table users (id varchar(32) NOT NULL PRIMARY KEY, name varchar(32) NOT NULL, hash varchar(256) NOT NULL, auth varchar(20) NOT NULL)")
 		if err != nil {
 			rows.Close()
 			return err
